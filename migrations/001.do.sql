f01e0050036a73d57c9db0b5be2253f935c69185
Jakub Gałecki <goathy.dev@gmail.com>
2023-04-03 20:48:48 +0000

    add missing fields

--- migrations/001.do.sql
+++ migrations/001.do.sql
@@ -1,5 +1,7 @@
 CREATE TABLE users (
     id TEXT PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT
 );
