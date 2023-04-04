657dd5d1b279c73df216f2803336649b4ebd51ba
Jakub Gałecki <goathy.dev@gmail.com>
2023-04-04 16:42:16 +0000

    add getUserByEmail query

---
+++ src/authentication/queries/get-user-by-email.query.mjs
@@ -0,0 +1,21 @@
+import { sql } from '@databases/sqlite'
+
+export async function getUserByEmailQuery (db, email) {
+  const [user] = await db.tx(async (tx) => {
+    return await tx.query(sql`
+        SELECT id,
+            email,
+            password,
+            first_name AS firstName,
+            last_name AS lastName
+        FROM users AS u
+        WHERE u.email = ${email}
+`)
+  })
+
+  console.log('debug: ', user)
+
+  return {}
+}
+
+export default getUserByEmailQuery
