3fb94370acfd4587d64decffbb04e1c536940a0a
Jakub Gałecki <goathy.dev@gmail.com>
2023-04-04 20:41:47 +0000

    refactor database plugin

--- src/database.mjs
+++ src/database.mjs
@@ -6,7 +6,11 @@ import fp from 'fastify-plugin'
 /**
  * @param {import('fastify').FastifyInstance} app
  */
-export default fp(async function database (app, opts = { database: 'db.sqlite' }) {
+export default fp(async function database (app, opts) {
+  if ('database' in opts === false) {
+    throw new Error("'opts.database' option is required")
+  }
+
   const db = connect(opts.database)
 
   app.decorate('db', db)
