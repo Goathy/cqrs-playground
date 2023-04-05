a1d629c70674dc9fdc508a1d5558b26a41356188
Jakub Gałecki <goathy.dev@gmail.com>
2023-04-05 14:28:41 +0000

    refactor test

--- src/authentication/commands/handlers/create-user-handler.test.mjs
+++ src/authentication/commands/handlers/create-user-handler.test.mjs
@@ -8,7 +8,7 @@ import { createUserHandler } from './create-user.handler.mjs'
 import UUID_REGEX from '../../../../tests/utils/uuid.mjs'
 import PASSWORD_REGEX from '../../../../tests/utils/password.mjs'
 
-test('create user', async ({ teardown, plan, fail, match, same }) => {
+test('create user', async ({ teardown, plan, match, same }) => {
   plan(3)
 
   const db = await prepare()
@@ -22,21 +22,17 @@ test('create user', async ({ teardown, plan, fail, match, same }) => {
 
   const command = createUserCommand(body)
 
-  try {
-    await createUserHandler(db, command)
+  await createUserHandler(db, command)
 
-    const [{ id, password, ...user }] = await db.query(sql`SELECT * FROM users`)
+  const [{ id, password, ...user }] = await db.query(sql`SELECT * FROM users`)
 
-    match(id, UUID_REGEX, 'id should be in uuid v4 format')
-    match(password, PASSWORD_REGEX, 'password do not match specified shape')
-    same(user, {
-      email: 'joe.doe@mail.co',
-      first_name: 'Joe',
-      last_name: 'Doe'
-    })
-  } catch (error) {
-    fail('throw an error')
-  }
+  match(id, UUID_REGEX, 'id should be in uuid v4 format')
+  match(password, PASSWORD_REGEX, 'password do not match specified shape')
+  same(user, {
+    email: 'joe.doe@mail.co',
+    first_name: 'Joe',
+    last_name: 'Doe'
+  })
 })
 
 test('create user | invalid command', async ({ teardown, plan, pass, fail }) => {
