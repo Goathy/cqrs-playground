b3b93ef8f9fdcf113d81a526fa7082ec5ae8c4e2
Jakub Gałecki <goathy.dev@gmail.com>
2023-04-05 14:19:40 +0000

    add prepare and clean helper functions

--- tests/utils/migration.mjs
+++ tests/utils/migration.mjs
@@ -2,6 +2,8 @@ import { exec as cexec } from 'node:child_process'
 import { promisify } from 'node:util'
 import { tmpdir } from 'node:os'
 import { join } from 'node:path'
+import connect from '@databases/sqlite'
+import { unlink } from 'node:fs/promises'
 
 export const TEST_DATABASE = join(tmpdir(), 'test.sqlite')
 
@@ -9,4 +11,13 @@ const exec = promisify(cexec)
 
 export async function runMigration () {
   await exec(`yarn postgrator --database ${TEST_DATABASE}`)
+}
+
+export async function prepare () {
+  await exec(`yarn postgrator --database ${TEST_DATABASE}`)
+  return connect(TEST_DATABASE)
+}
+
+export async function clean () {
+  await unlink(TEST_DATABASE)
 }
