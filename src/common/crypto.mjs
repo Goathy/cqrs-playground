8ab437563c5513f57a4f4f59cd7790b87796fde0
Jakub Gałecki <goathy.dev@gmail.com>
2023-04-12 23:04:50 +0000

    create wrapper class for crypto and bcrypt api

--- src/common/crypto.mjs
+++ src/common/crypto.mjs
@@ -2,9 +2,45 @@
 
 import { randomUUID } from 'node:crypto'
 import fp from 'fastify-plugin'
+import bcrypt from 'bcrypt'
+import { InternalError } from './errors/error.mjs'
+import assert from 'node:assert'
+
+export class CryptoSaltError extends InternalError {
+  constructor () {
+    super("error occur during 'salt' generation", 'CRYPTO_SALT_ERROR')
+  }
+}
+
+export class Crypto {
+  #crypto
+  // TODO: Pass opts object to Crypto constructor to configure genSalt method
+  constructor (crypto) {
+    assert.strictEqual(typeof crypto?.randomUUID, 'function', 'crypto do not implement \'randomUUID\' method')
+    assert.strictEqual(typeof crypto?.genSalt, 'function', 'crypto do not implement \'genSalt\' method')
+
+    this.#crypto = {
+      uuid: crypto.randomUUID,
+      genSalt: crypto.genSalt
+    }
+  }
+
+  uuid () {
+    return this.#crypto.uuid()
+  }
+
+  async genSalt () {
+    try {
+      return await this.#crypto.genSalt()
+    } catch (error) {
+      throw new CryptoSaltError()
+    }
+  }
+}
 
 export const crypto = {
-  uuid: randomUUID
+  uuid: randomUUID,
+  genSalt: async (rounds = 10, version = 'b') => await bcrypt.genSalt(rounds, version)
 }
 
 export default fp(async function (app) {
--- src/common/crypto.test.mjs
+++ src/common/crypto.test.mjs
@@ -1,16 +1,44 @@
 import { test } from 'tap'
-import { crypto } from './crypto.mjs'
+import { Crypto } from './crypto.mjs'
 import uuidRegex from '../../tests/utils/uuid.mjs'
+import c from 'node:crypto'
+import b from 'bcrypt'
 
 test('uuid', async (t) => {
   t.plan(1)
 
+  const crypto = new Crypto({ ...c, ...b })
   const uuid = crypto.uuid()
 
   t.match(uuid, uuidRegex)
 })
 
-test('genSalt', { todo: true })
-test('genSalt | throw error', { todo: true })
+test('genSalt', async (t) => {
+  t.plan(2)
+
+  const crypto = new Crypto({ ...c, ...b })
+  const salt = await crypto.genSalt()
+
+  t.equal(salt.length, 29)
+  t.match(salt, /^\$2b\$10\$/)
+})
+
+test('genSalt | throw error', async (t) => {
+  t.plan(1)
+
+  try {
+    const crypto = new Crypto({ ...c, genSalt: () => { throw new Error('test') } })
+    await crypto.genSalt()
+  } catch (error) {
+    const { message, code, type } = error
+
+    t.same({ message, code, type }, {
+      message: "error occur during 'salt' generation",
+      code: 'CRYPTO_SALT_ERROR',
+      type: 'INTERNAL'
+    })
+  }
+})
+
 test('genHash', { todo: true })
 test('genHash | throw error', { todo: true })
